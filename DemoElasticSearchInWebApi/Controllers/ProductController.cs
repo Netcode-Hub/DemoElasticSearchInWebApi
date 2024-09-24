6e2a3b0d40a559a2f5bc8e37aea32fb98d422782
Frederick Asante <Rastinga1995@hotmail.com>
2024-09-24 16:05:17 +0000

    Add project files.

---
+++ DemoElasticSearchInWebApi/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoElasticSearchInWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(ProductService service) : ControllerBase
+    {
+        [HttpPost("add-product")]
+        public async Task<IActionResult> AddProduct(Product model)
+        {
+           await service.IndexProductAsync(model);
+            return Ok();
+        }
+
+        [HttpGet("get-products")]
+        public async Task<IActionResult> GetProducts()
+        {
+            var results = await service.GetProductsAsync();
+            return Ok(results);
+        }
+
+        [HttpGet("search/{searchTerm}")]
+        public async Task<IActionResult> SearchProduct(string searchTerm)
+        {
+            var result = await service.SearchProduct(searchTerm);
+            return Ok(result);
+        }
+
+        [HttpGet("search-wildcard/{searchTerm}")]
+        public async Task<IActionResult> SearchProductWithWildCard(string searchTerm)
+        {
+            var result = await service.SearchProductWithWildCard(searchTerm);
+            return Ok(result);
+        }
+
+        [HttpGet("fuzzy-search/{searchTerm}")]
+        public async Task<IActionResult> FuzzyProductSearch(string searchTerm)
+        {
+            var result = await service.FuzzyProductSearch(searchTerm);
+            return Ok(result);
+        }
+    }
+}
---
+++ DemoElasticSearchInWebApi/ProductService.cs
@@ -0,0 +1,89 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.Nodes;
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public decimal? Price { get; set; }
+
+}
+namespace DemoElasticSearchInWebApi
+{
+    public class ProductService
+    {
+        private readonly ElasticsearchClient client;
+        private const string IndexName = "products";
+        public ProductService()
+        {
+            client = new ElasticsearchClient(new Uri("http://localhost:9200"));
+        }
+
+        public async Task IndexProductAsync(Product product)
+        {
+            var indexResponse = await client.IndexAsync(product, x => x.Index(IndexName));
+        }
+
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            var response = await client.SearchAsync<Product>(s => s.Index(IndexName));
+            return response.Hits.Select(hint => hint.Source).ToList();
+        }
+
+        public async Task<List<Product>> SearchProduct(string searchTerm)
+        {
+            var response = await client.SearchAsync<Product>(s => s
+                   .Index(IndexName)
+                   .Query(q => q
+                       .Bool(b => b
+                           .Should(
+                               //bs => bs.Match(m => m.Field(f => f.Id.ToString()).Query(searchTerm)),
+                               bs => bs.Match(m => m.Field(f => f.Name).Query(searchTerm))
+                           // bs => bs.Match(m => m.Field(f => f.Price).Query(decimal.Parse(searchTerm)))
+                           )
+                       )
+                   )
+               );
+
+            if (!response.IsValidResponse)
+                return [];
+            else
+                return response.Hits.Select(hint => hint.Source).ToList();
+        }
+
+        public async Task<List<Product>> SearchProductWithWildCard(string searchTerm)
+        {
+            var response = await client.SearchAsync<Product>(s => s
+                   .Index(IndexName)
+                   .Query(q => q
+                       .Wildcard(w => w
+                           .Field(f => f.Name)
+                           .Value($"*{searchTerm}*")
+                       )
+                   )
+               );
+
+            if (!response.IsValidResponse)
+                return [];
+            else
+                return response.Hits.Select(hint => hint.Source).ToList();
+        }
+
+        public async Task<List<Product>> FuzzyProductSearch(string searchTerm)
+        {
+            var searchResponse = await client.SearchAsync<Product>(s => s
+            .Index(IndexName)
+            .Query(q => q
+                .Fuzzy(f => f
+                    .Field(p => p.Name)
+                    .Value(searchTerm)
+                    .Fuzziness(new Fuzziness(2))
+                )
+            ));
+
+            if (!searchResponse.IsValidResponse)
+                return [];
+            else
+                return searchResponse.Hits.Select(hint => hint.Source).ToList();
+        }
+    }
+}
